dbe025b99f67a94babe34ef16c6ebe0eb91d0af3
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 19:18:34 +0100

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -1,35 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Cuestionando su petición...");
     let target = message.mentions.users.first() || message.author;
     
-    await message.channel.send("There is " + message.mentions.users.first() + " avatar!");
-    await message.channel.send({files: [
-        {
-            attachment: target.displayAvatarURL,
-            name: "wima.png"
-        }
-    ]});
-       
-    msg.delete();
-}
-
-module.exports.help = {
-    name: "wima"
-}
-
-
-
-// love you
-bot.on('message', message => {
-  if (message.content === 'love you') {
-      message.channel.send("I love you too ♥");
-      message.channel.send({
+    await message.channel.send(message.author + " loves you, " + message.mentions.users.first + "! ♥" );
+    await mmessage.channel.send({
             files: [
                 "./files/kokoro.png"
             ]
         })
-  }
-});
+}
 
+module.exports.help = {
+    name: "love"
+}
