e9d3ab7434804d5e59dfd8336af781dd0c477291
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 15:25:56 +0100

    Delete like.js

--- commands/like.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    message.react('').catch(e => console.log(e));
-}
-
-module.exports.help = {
-    name: "like"
