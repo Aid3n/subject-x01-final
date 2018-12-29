385e20e9e757c82ca58a146c0d26aba886ee93e2
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 14:32:14 +0100

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -5,8 +5,8 @@ module.exports.run = async (bot, message, args) => {
         const embed = new Discord.RichEmbed()
         .setColor('f4eb42')
         .setThumbnail('https://media.discordapp.net/attachments/478509969684299786/478509983659720704/AR01..jpg?width=480&height=480')
-        .setTitle("Aid3n/bot-discord-in-js")
-        .setURL("https://github.com/Aid3n/bot-discord-in-js")
+        .setTitle("Aid3n/subject-x01-final")
+        .setURL("https://github.com/Aid3n/subject-x01-final")
         .setDescription("You cand find my github project here!")
         .setAuthor('Aiden', 'https://media.discordapp.net/attachments/478509969684299786/478999662053949451/38618692.jpg', 'https://github.com/Aid3n')
         message.channel.sendEmbed(embed);
