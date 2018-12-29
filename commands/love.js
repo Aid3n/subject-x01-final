f0f6d8b0c3b0d283c26ed9919d300f86ba88f149
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 19:15:35 +0100

    Create love.js

---
+++ commands/love.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Cuestionando su petición...");
+    let target = message.mentions.users.first() || message.author;
+    
+    await message.channel.send("There is " + message.mentions.users.first() + " avatar!");
+    await message.channel.send({files: [
+        {
+            attachment: target.displayAvatarURL,
+            name: "wima.png"
+        }
+    ]});
+       
+    msg.delete();
+}
+
+module.exports.help = {
+    name: "wima"
+}
