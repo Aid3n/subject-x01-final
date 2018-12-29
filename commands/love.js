6f1d08771d790344f4ff4b1e11efc0ef8e8eb5dc
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 19:22:32 +0100

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.author;
     
-    await message.channel.send(message.author + " loves you, " + message.mentions.users.first() + "! ♥" );
+    await message.channel.send(message.author + " loves you, " + message.mentions.users.first() + " ! ♥" );
     await message.channel.send({
             files: [
                 "./files/kokoro.png"
