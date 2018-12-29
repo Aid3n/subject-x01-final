a87ca15ba37a7b59d9e4dc976c39e4a16d90591c
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 16:05:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ bot.on("message", async message => {
 // @bot
 bot.on('message', message => {
   if (message.content === '<@455725168556245003>') {
-      return message.channel.send("Mi prefijo és _**!**_\nDigo, <:ava:528587463695007764>");
+      return message.channel.send("Mi prefijo és _**!**_)
+      message.channel.send("Digo, ***A va.***);
   }
 });
 
