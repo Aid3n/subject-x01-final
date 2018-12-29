af3a7644f1bae5fd0901622d9fa44aebee6f6270
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-29 19:11:36 +0100

    Update wima.js

--- commands/wima.js
+++ commands/wima.js
@@ -1,9 +1,26 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send('This is your avatar! \n'+message.author.avatarURL).catch(e => console.log(e));
+    let msg = await message.channel.send("Cuestionando su petición...");
+    let target = message.mentions.users.first() || message.author;
+    
+    await message.channel.send("There is " + message.mentions.users.first() + " avatar!");
+    await message.channel.send({files: [
+        {
+            attachment: target.displayAvatarURL,
+            name: "wima.png"
+        }
+    ]});
+    
+    msg.delete();
 }
 
 module.exports.help = {
     name: "wima"
 }
+
+
+
+//module.exports.run = async (bot, message, args) => {
+  //  message.channel.send('This is your avatar! \n'+message.author.avatarURL).catch(e => console.log(e));
+//}
