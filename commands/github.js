eba2771962a3fcea1f4662914c309d65d8773b4e
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-12-30 13:14:42 +0100

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         .setTitle("Aid3n/subject-x01-final")
         .setURL("https://github.com/Aid3n/subject-x01-final")
         .setDescription("You cand find my github project here!")
-        .setAuthor('Aiden', 'https://media.discordapp.net/attachments/478509969684299786/478999662053949451/38618692.jpg', 'https://github.com/Aid3n')
+        .setAuthor('Aiden', 'https://media.discordapp.net/attachments/528177685273247747/528908575519801344/wima.png', 'https://github.com/Aid3n')
         message.channel.sendEmbed(embed);
         }
 }
