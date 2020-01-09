e6e5b39addf6598e670b9207c16ac8f87bae9acd
Aid3n <38618692+Aid3n@users.noreply.github.com>
2020-01-09 10:09:38 +0100

    Update wima.js

--- commands/wima.js
+++ commands/wima.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Cuestionando su petición...");
+    let msg = await message.channel.send("Pene su petición...");
     let target = message.mentions.users.first() || message.author;
     
     //await message.channel.send("There is " + message.mentions.users.first() + " avatar!");
